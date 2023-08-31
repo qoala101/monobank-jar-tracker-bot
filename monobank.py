1ded4b2d7209e49c5f5fb073c126cc9ac15ce27c
Volodymyr Hromakov <4y5t6r@gmail.com>
2023-08-31 17:23:05 +0300

    Added monobank API.

---
+++ monobank.py
@@ -0,0 +1,19 @@
+import httpx
+
+
+def GetShortIdFromJarUri(jar_uri: str):
+    uri_parts = jar_uri.split("/")
+    return uri_parts[-1]
+
+
+def FetchLongJarId(short_jar_id: str):
+    request_json = {"c": "hello", "clientId": short_jar_id, "Pc": "0"}
+    response = httpx.post(url="https://send.monobank.ua/api/handler", json=request_json)
+    response_json = response.json()
+    return response_json["extJarId"]
+
+
+def FetchJarAmount(long_jar_id):
+    response = httpx.get(url=f"https://api.monobank.ua/bank/jar/{long_jar_id}")
+    response_json = response.json()
+    return int(response_json["amount"])
